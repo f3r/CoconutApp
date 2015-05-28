9d2d81473505a772bc5a76c71b33f376ade1b772
Xiaopow <xiaoyang88@gmail.com>
2015-05-28 15:39:14 +0800

    bug with link from /tab/dash to /sign-in

--- www/js/app.js
+++ www/js/app.js
@@ -51,8 +51,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
+    views: {
+      'tab': { 
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+      }
+    }
   })
 
   // Each tab has its own nav history stack:
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,31 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+  
 })
 
 .controller('SignUpCtrl', function($scope) {})
+.controller('SignInCtrl', function($scope, $state) {
+  $scope.user = {};
+  $scope.form = {};
+
+  $scope.form.login = function(){
+    console.log("Logging In to Coconut Account!")
+
+    var url = "<%= ENV['URL'] %>" || "http://localhost:3000/";
+    var data = {
+      user: $scope.user
+    };
+
+    console.log(data)
+
+    // $http.post(url + "users/sign_in", data).success(function(response){
+    //   console.log("You Are Now Signed In To Coconut...");
+
+      $state.go('tab.dash'); //Need To Determine where to go after signin.
+    // });
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
